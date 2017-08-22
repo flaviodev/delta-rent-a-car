673f1dc1ed15ba64dadc4a6cac21b73936c9152d
flavio <fdsdev@gmail.com>
2017-08-22 15:11:26 -0300

    Refatoração fabricante

--- src/main/java/br/edu/faculdadedelta/rentacar/controller/FabricanteController.java
+++ src/main/java/br/edu/faculdadedelta/rentacar/controller/FabricanteController.java
@@ -1,79 +1,37 @@
 package br.edu.faculdadedelta.rentacar.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.faculdadedelta.rentacar.model.Fabricante;
 import br.edu.faculdadedelta.rentacar.repository.FabricanteRepository;
 
 @Controller
 @RequestMapping("/fabricantes")
-public class FabricanteController {
+public class FabricanteController extends AbstractCRUDController<Long, Fabricante, FabricanteRepository>{
 
-	@Autowired
-	private FabricanteRepository repository;
-	
-	@RequestMapping(value = "/novo", method = RequestMethod.GET)
-	public ModelAndView novo() {
-
-		ModelAndView mv = new ModelAndView("fabricante");
-		mv.addObject(new Fabricante());
-		
-		return mv;
+	@Override
+	public String getNomeControlador() {
+		return "fabricantes";
 	}
 	
-
-	@RequestMapping(value = "/salvar", method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Fabricante produto, Errors errors, RedirectAttributes redirectAttributes) {
-		
-		if(errors.hasErrors()) {
-			return new ModelAndView("fabricante");
-		}
-		
-		repository.save(produto);
-		
-		redirectAttributes.addFlashAttribute("message", "Fabricante Salvo con sucesso!");
-		
-		return new ModelAndView("redirect:/fabricantes/novo");
+	@Override
+	public String getNomeEntidade() {
+		return "fabricante";
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView listar() {
-
-		ModelAndView mv = new ModelAndView("listaFabricante");
-		
-		List<Fabricante> fabricantes = repository.findAll();
-		
-		mv.addObject("fabricantes", fabricantes);
-		
-		return mv;
+	@Override
+	public String getNomeListaEntidade() {
+		return "listaFabricante";
 	}
 
-	
-	@RequestMapping(value="/editar/{id}", method = RequestMethod.GET)
-	public ModelAndView editar(@PathVariable("id") Long id) {
-		Fabricante fabricante = repository.findOne(id);
-
-		ModelAndView mv = new ModelAndView("fabricante");
-		
-		mv.addObject("fabricante", fabricante);
-		
-		return mv;
+	@Override
+	public String getNomeMensagemEntidade() {
+		return "Fabricante";
 	}
 	
-	@RequestMapping(value="/excluir/{id}", method = RequestMethod.GET)
-	public ModelAndView excluir(@PathVariable("id") Long id) {
-		repository.delete(id);
-		
-		return new ModelAndView("redirect:/fabricantes");
+	@Override
+	public Fabricante getInstanciaEntidade() {
+		return new Fabricante();
 	}
 }
--- src/main/java/br/edu/faculdadedelta/rentacar/model/Fabricante.java
+++ src/main/java/br/edu/faculdadedelta/rentacar/model/Fabricante.java
@@ -1,7 +1,5 @@
 package br.edu.faculdadedelta.rentacar.model;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,20 +8,18 @@ import javax.persistence.Id;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
-public class Fabricante implements Serializable {
+public class Fabricante extends Model<Long> {
 
 	private static final long serialVersionUID = -1466134548885395261L;
-
 	
 	public Fabricante() {}
 
 	public Fabricante(Long id, String nome, String descricao) {
-		super();
-		this.id = id;
+		super(id);
 		this.nome = nome;
 		this.descricao = descricao;
 	}
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -34,15 +30,17 @@ public class Fabricante implements Serializable {
 	@NotBlank(message = "O campo descrição não pode ser vazio!")
 	private String descricao;
 
-
+	
+	@Override
 	public Long getId() {
 		return id;
 	}
-
+	
+	@Override
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -60,33 +58,8 @@ public class Fabricante implements Serializable {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Fabricante other = (Fabricante) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
-		return "Fabricante [id=" + id + ", nome=" + nome + "]";
+		return "Fabricante [id=" + getId() + ", nome=" + nome + "]";
 	}
 	
 }

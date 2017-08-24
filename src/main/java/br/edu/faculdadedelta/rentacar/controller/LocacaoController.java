f71bf4f2ae00003eeb71f32145b360bb3d733ec5
flavio <fdsdev@gmail.com>
2017-08-24 01:59:24 -0300

    conclusão da rotina de locação

--- src/main/java/br/edu/faculdadedelta/rentacar/controller/LocacaoController.java
+++ src/main/java/br/edu/faculdadedelta/rentacar/controller/LocacaoController.java
@@ -1,12 +1,18 @@
 package br.edu.faculdadedelta.rentacar.controller;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.faculdadedelta.rentacar.model.Carro;
 import br.edu.faculdadedelta.rentacar.model.Locacao;
@@ -17,7 +23,7 @@ import br.edu.faculdadedelta.rentacar.repository.MotoristaRepository;
 
 @Controller
 @RequestMapping("/"+LocacaoController.NOME_CONTROLADOR)
-public class LocacaoController extends CRUDControllerBase<Long, Locacao, LocacaoRepository>{
+public class LocacaoController extends ControllerBase<Long, Locacao, LocacaoRepository>{
 
 	protected static final String NOME_CONTROLADOR = "locacoes";
 	
@@ -38,6 +44,16 @@ public class LocacaoController extends CRUDControllerBase<Long, Locacao, Locacao
 	}
 
 	@Override
+	protected String getNomeTemplateListagem() {
+		return "listaLocacoes";
+	}
+
+	@Override
+	protected String getMensagemDeSucessoSalvar() {
+		return "Locação efetuada com sucesso!";
+	}
+	
+	@Override
 	public String getNomeEntidade() {
 		return "Locação";
 	}
@@ -49,13 +65,78 @@ public class LocacaoController extends CRUDControllerBase<Long, Locacao, Locacao
 	
 	@Override
 	public String[] getColunasListagem() {
-		return new String[]{"ID","Motorista", "Carro", "Valor da Diária", "Data de Locação"};
+		return new String[]{"ID","Motorista", "Carro", "Valor Diária", "Data Locação", "Data Devolução"};
 	}
 	
 	@Override
 	public String[] getAtributosListagem() {
-		return new String[]{"id","nomeDoMotorista","descricaoDoCarro", "valorDaDiaria", "dataDeLocacao"};
+		return new String[]{"id","nomeDoMotorista","descricaoDoCarro", "valorDaDiariaContratada", "dataDeLocacao", "dataDeDevolucao"};
 	}
+	
+	@Override
+	public ModelAndView novo() {
+		ModelAndView mv = super.novo(); 
+		
+		mv.addObject("isConsulta", false);
+		
+		return mv;
+	}
+	
+	@Override
+	public ModelAndView salvar(Locacao entidade, Errors errors, RedirectAttributes redirectAttributes) {
+		if(entidade!=null && entidade.getCarro()!=null) {
+			entidade.setValorDaDiariaContratada(entidade.getCarro().getValorDaDiaria());
+		}
+		
+		return super.salvar(entidade, errors, redirectAttributes);
+	}
+	
+	@RequestMapping(value="/consultar/{id}", method = RequestMethod.GET)
+	public ModelAndView consultar(@PathVariable("id") Long id) {
+		Locacao locacao = getRepositorio().findOne(id);
+
+		ModelAndView mv = new ModelAndView(getNomeTemplateEdicao());
+		
+		mv.addObject(getNomeTemplateEdicao(), locacao);
+		mv.addObject("isConsulta", true);
+		
+		if(locacao!=null && locacao.getDataDeDevolucao()==null) {
+			long diferenca = Math.abs(new Date().getTime() - locacao.getDataDeLocacao().getTime());
+			long diferencaEmDias = diferenca / (24 * 60 * 60 * 1000);
+			diferencaEmDias++;
+ 
+			mv.addObject("diasLocados", diferencaEmDias);
+			mv.addObject("valorAPagar", locacao.getValorDaDiariaContratada().multiply(new BigDecimal(diferencaEmDias)));
+		}
+		
+		return mv;
+	}	
+
+	@RequestMapping(value="/devolver/{id}", method = RequestMethod.GET)
+	public ModelAndView devolver(@PathVariable("id") Long id) {
+		Locacao locacao = getRepositorio().findOne(id);
+
+		ModelAndView mv = new ModelAndView(getNomeTemplateEdicao());
+		
+		mv.addObject(getNomeTemplateEdicao(), locacao);
+		mv.addObject("isConsulta", true);
+		
+		if(locacao!=null && locacao.getDataDeDevolucao()==null) {
+			locacao.setDataDeDevolucao(new Date());
+			
+			long diferenca = Math.abs(new Date().getTime() - locacao.getDataDeLocacao().getTime());
+			long diferencaEmDias = diferenca / (24 * 60 * 60 * 1000);
+			diferencaEmDias++;
+ 
+			locacao.setValorTotal(locacao.getValorDaDiariaContratada().multiply(new BigDecimal(diferencaEmDias)));
+
+			getRepositorio().save(locacao);
+			
+			mv.addObject("mensagem", "Devolução registrada com sucesso!");
+		}
+		
+		return mv;
+	}	
 	
 	@ModelAttribute("todosMotoristas")
 	public List<Motorista> todosMotoristas() {
--- src/main/java/br/edu/faculdadedelta/rentacar/model/Locacao.java
+++ src/main/java/br/edu/faculdadedelta/rentacar/model/Locacao.java
@@ -1,7 +1,6 @@
 package br.edu.faculdadedelta.rentacar.model;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,15 +22,15 @@ public class Locacao extends EntidadeBase<Long> {
 	private static final long serialVersionUID = -3475674544565344461L;
 	
 	private static final SimpleDateFormat FORMATADOR_DATA =  new SimpleDateFormat("dd/MM/yyyy");
-	private static final DecimalFormat FORMATADOR_MOEDA = new DecimalFormat("R$ #,###.00");
 	
 	public Locacao() {
 		this.dataDeLocacao = new Date();
 	}
-	
-	public Locacao(Long id, BigDecimal valorTotal, Date dataDeLocacao, Date dataDeDevolucao, Motorista motorista,
-			Carro carro) {
+
+	public Locacao(Long id, BigDecimal valorDaDiariaContratada, BigDecimal valorTotal, Date dataDeLocacao,
+			Date dataDeDevolucao, Motorista motorista, Carro carro) {
 		this.id = id;
+		this.valorDaDiariaContratada = valorDaDiariaContratada;
 		this.valorTotal = valorTotal;
 		this.dataDeLocacao = dataDeLocacao;
 		this.dataDeDevolucao = dataDeDevolucao;
@@ -42,6 +41,9 @@ public class Locacao extends EntidadeBase<Long> {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NumberFormat(pattern = "#,##0.00")
+	private BigDecimal valorDaDiariaContratada;
 	
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valorTotal;
@@ -78,6 +80,14 @@ public class Locacao extends EntidadeBase<Long> {
 
 	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
+	}
+
+	public BigDecimal getValorDaDiariaContratada() {
+		return valorDaDiariaContratada;
+	}
+
+	public void setValorDaDiariaContratada(BigDecimal valorDaDiariaContratada) {
+		this.valorDaDiariaContratada = valorDaDiariaContratada;
 	}
 
 	public Date getDataDeLocacao() {
@@ -124,18 +134,23 @@ public class Locacao extends EntidadeBase<Long> {
 		return texto.toString();
 	}	
 	
-	public String getNomeDoMotorista() {
+	public String getNomeDoMotoristaComCpf() {
 		return motorista!=null ? motorista.getTextoApresentacao() : null;
 	}
 
-	public String getValorDaDiaria() {
-		return carro!=null && carro.getValorDaDiaria()!=null ? FORMATADOR_MOEDA.format(carro.getValorDaDiaria()) : null;
-	}
-	
-	public String getDescricaoDoCarro() {
-		return carro!=null ? carro.getTextoApresentacao() : null;
+	public String getNomeDoMotorista() {
+		return motorista!=null ? motorista.getNome() : null;
 	}
 
+	
+	public String getDescricaoDoCarro() {
+		return carro!=null ? carro.getTextoApresentacaoSimplificado() : null;
+	}
+
+	public String getDescricaoDoCarroComDiaria() {
+		return carro!=null ? carro.getTextoApresentacao() : null;
+	}	
+	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();

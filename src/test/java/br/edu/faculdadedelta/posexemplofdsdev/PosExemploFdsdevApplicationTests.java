02fe19261474bead92e19eb2ced55071321f5e25
Flávio de Souza <fdsdev@gmail.com>
2018-03-14 23:38:04 -0300

    Update PosExemploFdsdevApplicationTests.java

--- src/test/java/br/edu/faculdadedelta/posexemplofdsdev/PosExemploFdsdevApplicationTests.java
+++ src/test/java/br/edu/faculdadedelta/posexemplofdsdev/PosExemploFdsdevApplicationTests.java
@@ -1,5 +1,7 @@
 package br.edu.faculdadedelta.posexemplofdsdev;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;

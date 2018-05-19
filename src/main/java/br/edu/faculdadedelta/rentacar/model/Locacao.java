5f0b4459fe83387285e2f0a48ac77f2657dbd5ce
Flávio de Souza <fdsdev@gmail.com>
2018-05-18 21:12:57 -0300

    Update Locacao.java

--- src/main/java/br/edu/faculdadedelta/rentacar/model/Locacao.java
+++ src/main/java/br/edu/faculdadedelta/rentacar/model/Locacao.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Transient;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,6 +22,7 @@ public class Locacao extends EntidadeBase<Long> {
 
 	private static final long serialVersionUID = -3475674544565344461L;
 	
+	@Transient
 	private final SimpleDateFormat FORMATADOR_DATA =  new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Locacao() {
